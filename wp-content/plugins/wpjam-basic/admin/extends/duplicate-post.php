4894bf31d9eee65fc51d9318f43590c17c3032e8
Lucky21418 <deng.heng@qq.com>
2020-06-04 18:39:22 +0800

    new
    new

--- wp-content/plugins/wpjam-basic/admin/extends/duplicate-post.php
+++
@@ -1,9 +0,0 @@
-<?php
-add_action('wpjam_builtin_page_load', function ($screen_base, $current_screen){
-	if($screen_base == 'edit'){
-		if(is_post_type_viewable($current_screen->post_type)){
-			$post_type	= $current_screen->post_type;
-			include WPJAM_BASIC_PLUGIN_DIR.'admin/hooks/post-duplicate.php';
-		}
-	}
-}, 1, 2);
